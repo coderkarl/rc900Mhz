a6ac61378745f7aa1546b766d48ce95274a0ef73
Karl Kirsch <21990134+coderkarl@users.noreply.github.com>
2020-07-05 16:18:24 -0500

    Receiver to sabertooth over Serial1 Tx pin. Remove PPM from receiver.

--- rc_900_rx_saberserial/rc_900_rx_saberserial.ino
+++ rc_900_rx_saberserial/rc_900_rx_saberserial.ino
@@ -23,16 +23,34 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 // Blinky on receipt
 #define LED 13
 
-//////////////////////PPM CONFIGURATION///////////////////////////////
-#define NUM_CHANNELS 8
-#define FRAME_LENGTH 20000
-#define DEFAULT_PULSE_LENGTH 1500
-#define PULSE_LOW_LENGTH 400
-// NOTE: the output pin must be Arduino pin 5 on a Feather32U4 (or Leonardo)
-#define sigPin 5  //set PPM signal output pin on the arduino
+///////////////////////////////////////////////////////////////
+#include <USBSabertooth_NB.h>
+// Sabertooth Serial directly from radio receiver
+USBSabertoothSerial saberSerial(Serial1);
+USBSabertooth ST(saberSerial, 128); // Address 128, and use saberSerial as the serial port.
+
+int16_t steer_pwm = 1500;
+int16_t speed_pwm = 1500;
+int16_t blade_pwm = 1500;
+int16_t auto_pwm = 1500;
+
+int16_t scaled_speed_power = 0;
+int16_t scaled_steer_power = 0;
+
+long timeCMD;
+#define CMD_PERIOD 100
+
+// Read "PPM" data directly from radio packets
+//RC CHANNELS
+#define RC_STEER 0
+#define RC_SPEED 1
+#define RC_BLADE 3
+#define RC_AUTO 2
 
 /*this array holds the servo values for the ppm signal
  change theese values in your code (usually servo values move between 1000 and 2000)*/
+#define NUM_CHANNELS 8
+#define DEFAULT_PULSE_LENGTH 1500
 uint16_t ppm[NUM_CHANNELS];
 //////////////////////////////////////////////////////////////////
 
@@ -46,6 +64,11 @@ void setup()
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
+
+  Serial1.begin(9600);
+  ST.drive(scaled_speed_power);
+  ST.turn(scaled_steer_power);
+  timeCMD = millis();
 
   Serial.begin(115200);
   /*while (!Serial) {
@@ -81,39 +104,6 @@ void setup()
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
-
-  //////////////////////PPM SETUP///////////////////////////////
-  //initiallize default ppm values
-  // 0 - Right Horz, turn
-  // 1 - Right Vert, fwd/rev
-  // 2 - Left Vert, throttle, auto mode
-  // 3 - Left Horz, blade deadman switch
-
-  pinMode(sigPin, OUTPUT);
-  digitalWrite(sigPin, HIGH);
-
-  cli();
-  for(int i = 0; i < NUM_CHANNELS; ++i){
-    ppm[i]= DEFAULT_PULSE_LENGTH;
-  }
-
-  // Stop the timer/counter
-  TCCR3A = TCCR3B = 0;
-  // Set the low pulse length.  This will never change.
-  OCR3A = PULSE_LOW_LENGTH;
-  // Time until first falling edge, can be any value greater than PULSE_LOW_LENGTH.
-  ICR3 = PULSE_LOW_LENGTH + 1;
-  // Clear the count
-  TCNT3 = 0;
-  // Fast PWM; TOP = ICR3; set OC3A on compare match, clear on TOP; prescale = /8 (1MHz on Feather32u4)
-  TCCR3A = _BV(COM3A1) | _BV(COM3A0) | _BV(WGM31);
-  TCCR3B = _BV(WGM33) | _BV(WGM32) | _BV(CS31);
-  // Clear any pending timer overflow interrupt flag
-  TIFR3 = _BV(TOV3);
-  // Interrupt on overflow (at TOP, immediately after falling edge)
-  TIMSK3 = _BV(TOIE3);
-  sei();
-  //////////////////////////////////////////////////////////////
 }
 
 void loop()
@@ -121,6 +111,32 @@ void loop()
   if(timeSince(timeNewPacket) > PACKET_TIMEOUT)
   {
     set_default_ppm();
+  }
+
+  if(millis() - timeCMD > CMD_PERIOD)
+  {
+    timeCMD = millis();
+    steer_pwm = ppm[RC_STEER];
+    speed_pwm = ppm[RC_SPEED];
+    blade_pwm = ppm[RC_BLADE];
+    auto_pwm = ppm[RC_AUTO];
+    
+    if(auto_pwm < 1700)
+    {
+      if(abs(speed_pwm - 1500) <= 500 && abs(steer_pwm-1500) <= 500 )
+      {
+        // power -2047 to 2047
+        scaled_speed_power = ((float)(speed_pwm - 1500))*1.5;
+        scaled_steer_power = ((float)(steer_pwm - 1500))*1.5;
+      }
+      else
+      {
+        scaled_speed_power = 0;
+        scaled_steer_power = 0;
+      }
+      ST.drive(scaled_speed_power);
+      ST.turn(-scaled_steer_power);
+    }
   }
   
   if (rf95.available())
@@ -151,9 +167,8 @@ void loop()
           {
             pulse_val = 1999;
           }
-          cli();
           ppm[k] = pulse_val; //Consider hard coding to 1500 and see if it removes hiccups
-          sei();
+          
           Serial.print(pulse_val);
           Serial.print(",");
         }
@@ -190,31 +205,7 @@ uint16_t timeSince(uint16_t startTime)
 
 void set_default_ppm()
 {
-  cli();
   for(int i = 0; i < NUM_CHANNELS; ++i){
     ppm[i]= DEFAULT_PULSE_LENGTH;
   }
-  sei();
-}
-
-// The Overflow interrupt occurs when the counter reaches the TOP value in ICR3.
-// The hardware automatically emits a falling edge at this time.
-// The hardware will automatically emit a rising edge later based on the value in OCR3A.
-// The ISR just needs to store the time of the next falling edge in ICR3.
-ISR(TIMER3_OVF_vect){
-    static byte cur_chan_num = 0;
-    static unsigned int calc_rest = 0;  // Microseconds since start of frame
-  
-    if(cur_chan_num >= NUM_CHANNELS){
-      // If all channels have been output, then idle for the remainder of the frame
-      ICR3 = FRAME_LENGTH - calc_rest;
-      cur_chan_num = 0;
-      calc_rest = 0;
-    }
-    else{
-      // Set the time for the next falling edge.
-      ICR3 = ppm[cur_chan_num];
-      calc_rest += ppm[cur_chan_num];
-      cur_chan_num++;
-    }     
 }
